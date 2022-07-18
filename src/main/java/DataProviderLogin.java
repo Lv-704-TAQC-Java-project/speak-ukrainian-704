58e88a4bf7f2a44826f700f67fdbb9160a522e43
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-07-18 19:47:31 +0300

    corrected login data

--- src/main/java/DataProviderLogin.java
+++ src/main/java/DataProviderLogin.java
@@ -1,11 +1,28 @@
 import org.testng.annotations.DataProvider;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class DataProviderLogin {
 
     @DataProvider(name = "validLoginAdmin")
     public static Object[][] validLoginAdmin() {
+        Properties prop = new Properties();
+        FileInputStream ip = null;
+        try {
+            ip = new FileInputStream("/Users/myroslavavynokur/ATQC_Course/speak-ukrainian-704/config.properties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            prop.load(ip);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new Object[][]{
-                {"admin@gmail.com", "admin", "Ви успішно залогувалися!"}
+                {prop.getProperty("email"), prop.getProperty("password"), "Ви успішно залогувалися!"}
         };
     }
 
