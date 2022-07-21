79c21a305215a316a520a4553cbc72b833109877
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-07-21 18:23:47 +0300

    EditProfileModalComponent added

--- src/main/java/pages/ProfilePage.java
+++ src/main/java/pages/ProfilePage.java
@@ -3,10 +3,15 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import pages.components.EditProfileModalComponent;
+import pages.components.LoginModalComponent;
 
 
 public class ProfilePage extends BasePage {
     private WebElement currentUserEmailField;
+    private WebElement editProfileButton;
+    private EditProfileModalComponent editProfileModalComponent;
+
 
 
     public ProfilePage(WebDriver driver) {
@@ -19,5 +24,25 @@ public class ProfilePage extends BasePage {
             currentUserEmailField = driver.findElement(By.xpath("//div[@class='user-email-data']"));
         }
         return currentUserEmailField;
+    }
+
+    public WebElement getEditProfileButton() {
+        if (editProfileButton == null) {
+            waitVisibilityOfElement(By.xpath("//button[contains(@class, 'ant-btn-text button')]"));
+            editProfileButton = driver.findElement(By.xpath("//button[contains(@class, 'ant-btn-text button')]"));
+        }
+        return editProfileButton;
+    }
+
+    public EditProfileModalComponent getEditProfileModalComponent() {
+        if (editProfileModalComponent == null) {
+            editProfileModalComponent = new EditProfileModalComponent(driver);
+        }
+        return editProfileModalComponent;
+    }
+
+    public EditProfileModalComponent clickEditProfileButton() {
+        getEditProfileButton().click();
+        return getEditProfileModalComponent();
     }
 }
