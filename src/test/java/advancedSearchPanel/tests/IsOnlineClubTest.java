322cf60601bb32096785bc84df297e3d8f9822b1
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-07-25 18:23:31 +0300

    Adding of isOnlineClubTest

---
+++ src/test/java/advancedSearchPanel/tests/IsOnlineClubTest.java
@@ -0,0 +1,28 @@
+package advancedSearchPanel.tests;
+
+import login.tests.runners.BaseTestRunner;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.clubs.AdvancedSearchPanelComponent;
+import pages.clubs.ClubPageComponent;
+import pages.clubs.ClubsPage;
+
+import java.util.List;
+
+public class IsOnlineClubTest extends BaseTestRunner {
+    @Test
+    public void checkIsAllClubsIsAvailableOnline() {
+        ClubsPage clubsPage = new HomePage(driver)
+                .clickAdvancedSearchButton();
+
+        AdvancedSearchPanelComponent advancedSearchPanelComponent = clubsPage.getAdvancedSearchPanelComponent();
+
+        advancedSearchPanelComponent.isOnlineCheckboxClick();
+
+        ClubPageComponent clubPageComponent = clubsPage.getClubPageComponent();
+        List<WebElement> cards = clubPageComponent.getListOfCards();
+        Assert.assertTrue(clubPageComponent.isAllClubsAvailableOnline(cards), "Some of clubs are not available online");
+    }
+}
