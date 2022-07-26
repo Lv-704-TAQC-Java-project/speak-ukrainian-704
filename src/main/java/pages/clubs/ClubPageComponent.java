b894f63a5239c7eff77c5d56ddbd50bcb19b4bdd
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-07-26 13:00:24 +0300

    add test by rating

--- src/main/java/pages/clubs/ClubPageComponent.java
+++ src/main/java/pages/clubs/ClubPageComponent.java
@@ -12,8 +12,10 @@ public class ClubPageComponent extends BasePage {
 
     private WebElement listCard;
     private WebElement blockCard;
-    private WebElement cardBody;
+    List<WebElement> starRatingZeroList;
+    List<WebElement> starRatingFullList;
     List<WebElement> cardNamesList;
+    List<WebElement> centerNamesList;
 
 
     public ClubPageComponent(WebDriver driver) {
@@ -36,8 +38,40 @@ public class ClubPageComponent extends BasePage {
         return blockCard;
     }
 
-    public List<WebElement> getListOfCardNames() {
+//    public List<WebElement> getStarRatingZeroList() {
+//        WebElement cardBody = driver.findElement(By.xpath("//div[contains(@class, 'card-body')]"));
+//        starRatingZeroList = cardBody.findElements(By.className("ant-rate-star ant-rate-star-zero"));
+//        return starRatingZeroList;
+//    }
+//
+//    public List<WebElement> getStarRatingFullList() {
+//        WebElement cardBody = driver.findElement(By.xpath("//div[contains(@class, 'card-body')]"));
+//        starRatingFullList = cardBody.findElements(By.className("ant-rate-star ant-rate-star-full"));
+//        return starRatingFullList;
+//    }
+
+//    public List<WebElement> getStarRatingZeroList() {
+//        return driver.findElements(By.xpath("//div[contains(@class, 'card-body')]//li[contains(@class, 'zero')]"));
+//    }
+//
+//    public List<WebElement> getStarRatingFullList() {
+//        return driver.findElements(By.xpath("//div[contains(@class, 'card-body')]//li[contains(@class, 'full')]"));
+//    }
+
+    public List<WebElement> getStarRatingZeroList() {
+        return driver.findElements(By.xpath("//div[2]/div[1]//ul/li[@class = 'ant-rate-star ant-rate-star-zero']"));
+    }
+
+    public List<WebElement> getStarRatingFullList() {
+        return driver.findElements(By.xpath("//div[2]/div[1]//ul/li[@class = 'ant-rate-star ant-rate-star-full']"));
+    }
+
+    public List<WebElement> getCardNamesList() {
         return driver.findElements(By.xpath("//div[contains(@class, 'card-body')]//div[contains(@class, 'name')]"));
+    }
+
+    public List<WebElement> getCenterNamesList() {
+        return driver.findElements(By.xpath("//div[contains(@class, 'card-body')]//div[contains(@class, 'center-name')]"));
     }
 
     public List<String> getUpperCaseTextFromWebElementsList(List<WebElement> webElementList) {
--- src/test/java/login/tests/SortClubTest.java
+++ src/test/java/login/tests/SortClubTest.java
@@ -2,6 +2,7 @@ package login.tests;
 
 import jdk.jfr.Description;
 import login.tests.runners.BaseTestRunner;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -9,13 +10,14 @@ import pages.HomePage;
 import pages.clubs.ClubPageComponent;
 import pages.clubs.ClubsPage;
 import pages.clubs.SortClubComponent;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SortClubTest extends BaseTestRunner {
 
     @Test
-    @Description("Verify that cards are displayed in a block view after clicking on blockViewButton")
+    @Description("Verify that cards are displayed in a block view")
     public void blockCardViewIsDisplayed() {
         ClubsPage clubsPage = new HomePage(driver)
                 .clickAdvancedSearchButton();
@@ -30,7 +32,7 @@ public class SortClubTest extends BaseTestRunner {
     }
 
     @Test
-    @Description("Verify that cards are displayed in a list view after clicking on listViewButton")
+    @Description("Verify that cards are displayed in a list view")
     public void listCardViewIsDisplayed() {
         ClubsPage clubsPage = new HomePage(driver)
                 .clickAdvancedSearchButton();
@@ -45,7 +47,7 @@ public class SortClubTest extends BaseTestRunner {
     }
 
     @Test
-    @Description("Verify that cards are sorted in ascending order after clicking on sortByABCButton and arrowDownButton")
+    @Description("Verify that cards are sorted in ascending order by ABC")
     public void sortByAbcASCTest() {
         ClubsPage clubsPage = new HomePage(driver)
                 .clickAdvancedSearchButton();
@@ -60,7 +62,7 @@ public class SortClubTest extends BaseTestRunner {
         ClubPageComponent clubPageComponent = clubsPage.getClubPageComponent();
 
         List<String> namesActual = clubPageComponent
-                .getUpperCaseTextFromWebElementsList(clubPageComponent.getListOfCardNames());
+                .getUpperCaseTextFromWebElementsList(clubPageComponent.getCardNamesList());
 
         for (String string : namesActual) {
             softAssert.assertTrue(string.startsWith("A"));
@@ -70,7 +72,7 @@ public class SortClubTest extends BaseTestRunner {
     }
 
     @Test
-    @Description("Verify that cards are sorted in descending order after clicking on sortByABCButton and arrowUpButton")
+    @Description("Verify that cards are sorted in descending order by ABC")
     public void sortByAbcDECTest() {
         ClubsPage clubsPage = new HomePage(driver)
                 .clickAdvancedSearchButton();
@@ -82,7 +84,7 @@ public class SortClubTest extends BaseTestRunner {
         ClubPageComponent clubPageComponent = clubsPage.getClubPageComponent();
 
         List<String> cardNamesDECActual = clubPageComponent
-                .getUpperCaseTextFromWebElementsList(clubPageComponent.getListOfCardNames());
+                .getUpperCaseTextFromWebElementsList(clubPageComponent.getCardNamesList());
 
         SoftAssert softAssert = new SoftAssert();
         List<String> cardNamesDECExpected = new ArrayList<>();
@@ -96,6 +98,51 @@ public class SortClubTest extends BaseTestRunner {
         softAssert.assertEquals(cardNamesDECActual.toString(), cardNamesDECExpected.toString());
         softAssert.assertAll();
     }
+
+    @Test
+    @Description("Verify that cards are sorted in descending order by rating")
+    public void sortByRatingDECTest() {
+        ClubsPage clubsPage = new HomePage(driver)
+                .clickAdvancedSearchButton();
+
+        SortClubComponent sortClubComponent = clubsPage.getSortClubComponent()
+                .sortByRatingButtonClick();
+
+        boolean zeroStar = clubsPage.getClubPageComponent()
+                .getStarRatingZeroList().get(1).isDisplayed();
+
+        sortClubComponent.arrowUpButtonClick();
+
+        List<WebElement> starsAmountList = clubsPage.getClubPageComponent()
+                .getStarRatingFullList();
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertFalse(zeroStar);
+        softAssert.assertEquals(starsAmountList.size(), 5);
+
+        softAssert.assertAll();
+    }
+
+//    @Test
+//    @Description("Verify that cards are sorted in ascending order by rating")
+//    public void sortByRatingASCTest() {
+//        ClubsPage clubsPage = new HomePage(driver)
+//                .clickAdvancedSearchButton();
+//
+//        SortClubComponent sortClubComponent = clubsPage.getSortClubComponent()
+//                .sortByRatingButtonClick()
+//                .arrowUpButtonClick()
+//                .arrowDownButtonClick();
+//
+//
+//        List<WebElement> starsAmountList = clubsPage.getClubPageComponent()
+//                .getStarRatingZeroList();
+//
+//        SoftAssert softAssert = new SoftAssert();
+//
+//        softAssert.assertEquals(starsAmountList.size(), 30);
+//        softAssert.assertAll();
+//    }
 
 }
 
