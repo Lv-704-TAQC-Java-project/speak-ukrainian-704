d06b6783a6bf6afc4d8846f5256a5e91719cb0e6
Olenka <olenkadashinich@gmail.com>
2022-08-01 09:11:27 +0300

    Added password check in RegistrationTest

--- src/test/java/login/tests/RegistrationTest.java
+++ src/test/java/login/tests/RegistrationTest.java
@@ -11,31 +11,42 @@ public class RegistrationTest extends BaseTestRunner {
     @DataProvider(name = "invalidDataRegistration")
     public Object[][] invalidNamesDataRegistration() {
         return new Object[][]{
-                {"  ", "  ", "72540"},
+                {"  ", "  ", "72540", "@qn.com", "1234", "123"},
         };
     }
 
 
     @Test(dataProvider = "invalidDataRegistration")
-    public void invalidDataRegistration(String lastName, String name, String phone) {
+    public void invalidDataRegistration(String lastName, String name, String phone, String email, String password, String confirmPassword) {
         RegistrationModelComponent registrationComponent = new HomePage(driver)
                 .getHeader()
                 .openGuestProfileMenu()
                 .clickRegistrationButton()
                 .fillInLastName(lastName)
                 .fillInName(name)
-                .fillInPhone(phone);
+                .fillInPhone(phone)
+                .fillInEmail(email)
+                .fillInPassword(password)
+                .fillInConfirmPassword(confirmPassword);
         String errorMessages = registrationComponent.getAllErrorMessages();
         SoftAssert softAssert = new SoftAssert();
 
-        String invalidName = "Ім`я повинно починатися і закінчуватися літерою";
-        String invalidLastName = "Прізвище повинно починатися і закінчуватися літерою";
-        String invalidPhone = "Телефон не відповідає вказаному формату";
+        String invalidNameMessage = "Ім`я повинно починатися і закінчуватися літерою";
+        String invalidLastNameMessage = "Прізвище повинно починатися і закінчуватися літерою";
+        String invalidPhoneMessage = "Телефон не відповідає вказаному формату";
+        String invalidEmailMessage = "Некоректний формат email";
+        String invalidPasswordMessage = "Пароль повинен містити хоча б одну маленьку літеру";
+        String invalidConfirmPasswordMessage = "Значення поля ‘Підтвердити пароль’ має бути еквівалентним значенню поля ‘Пароль’";
 
 
-        softAssert.assertTrue(errorMessages.contains(invalidLastName), "Invalid error name message");
-        softAssert.assertTrue(errorMessages.contains(invalidName), "Invalid error last name message");
-        softAssert.assertTrue(errorMessages.contains(invalidPhone), "Invalid error phone message");
+
+        softAssert.assertTrue(errorMessages.contains(invalidLastNameMessage), "Invalid error name message");
+        softAssert.assertTrue(errorMessages.contains(invalidNameMessage), "Invalid error last name message");
+        softAssert.assertTrue(errorMessages.contains(invalidPhoneMessage), "Invalid error phone message");
+        softAssert.assertTrue(errorMessages.contains(invalidEmailMessage), "Invalid error email message");
+        softAssert.assertTrue(errorMessages.contains(invalidPasswordMessage), "Invalid error password message");
+        softAssert.assertTrue(errorMessages.contains(invalidConfirmPasswordMessage), "Invalid error confirm password message");
+
 
 
         softAssert.assertAll();
